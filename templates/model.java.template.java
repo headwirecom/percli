2abe0b04ebeacd0e39161bea2c7a43593c90b12c
Nicholaus Perez <hea02160@adobe.com>
2017-10-27 15:20:02 -0700

    Add functionality for collections to the sling models

--- templates/model.java.template.java
+++ templates/model.java.template.java
@@ -8,6 +8,7 @@ import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
